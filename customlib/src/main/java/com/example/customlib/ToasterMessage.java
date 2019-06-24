d50c2db04490142b33f76e3245b8acac1a9af612
narendrakumar.d <desamnarendrakumar@gmail.com>
2019-06-24 15:06:11 +0530

    Committed By Adding Activity Toast

--- customlib/src/main/java/com/example/customlib/ToasterMessage.java
+++ customlib/src/main/java/com/example/customlib/ToasterMessage.java
@@ -1,10 +1,15 @@
 package com.example.customlib;
 
+import android.app.Activity;
 import android.content.Context;
 import android.widget.Toast;
 
 public class ToasterMessage {
     public static void showToast(Context c, String message){
         Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+    }
+
+    public static void showToast(Activity a, String message){
+        Toast.makeText(a,message,Toast.LENGTH_SHORT).show();
     }
 }
