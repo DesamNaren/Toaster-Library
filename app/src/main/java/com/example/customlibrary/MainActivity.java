4196434486bfd5561dcb3953b511fecd18dbe591
narendrakumar.d <desamnarendrakumar@gmail.com>
2019-06-25 17:39:42 +0530

    Commit by adding alerts

--- app/src/main/java/com/example/customlibrary/MainActivity.java
+++ app/src/main/java/com/example/customlibrary/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.customlibrary;
 
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.customlib.ToasterMessage;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showToast(View view) {
+        ToasterMessage.showToast(this, "Hey Toast");
+    }
+
+    public void showSnackBar(View view) {
+        ToasterMessage.showSnackBar(findViewById(R.id.root_layout), "Hey Snackbar", Snackbar.LENGTH_SHORT);
+    }
+
+    public void showSnackBarColor(View view) {
+        ToasterMessage.showSnackBar(findViewById(R.id.root_layout), "Hey Snackbar", Snackbar.LENGTH_SHORT, R.color.colorPrimary);
+    }
+
+    public void showOKAlert(View view) {
+        ToasterMessage.showAlertOK(this, getResources().getString(R.string.app_name), "Hey Alert","OK", true);
+    }
+
+    public void showOKCanAlert(View view) {
+        ToasterMessage.showAlertOKCancel(this, getResources().getString(R.string.app_name), "Hey Alert","OK", "Cancel", true);
     }
 }
--- customlib/src/main/java/com/example/customlib/ToasterMessage.java
+++ customlib/src/main/java/com/example/customlib/ToasterMessage.java
@@ -1,15 +1,77 @@
 package com.example.customlib;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class ToasterMessage {
-    public static void showToast(Context c, String message){
-        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+
+    //Use context
+    public static void showToast(Context c, String message) {
+        Toast.makeText(c, message, Toast.LENGTH_SHORT).show();
     }
 
-    public static void showToast(Activity a, String message){
-        Toast.makeText(a,message,Toast.LENGTH_SHORT).show();
+
+    //Use activity context
+    public static void showToast(Activity a, String message) {
+        Toast.makeText(a, message, Toast.LENGTH_SHORT).show();
     }
+
+    //Pass your root view
+    public static void showSnackBar(View view, String message, int duration) {
+        Snackbar.make(view, message, duration).show();
+    }
+
+    //Pass your root view and background color
+    public static void showSnackBar(View view, String message, int duration, int color) {
+        Snackbar snackbar;
+        snackbar = Snackbar.make(view, message, duration);
+        View snackBarView = snackbar.getView();
+        snackBarView.setBackgroundColor(color);
+
+        TextView textView = snackBarView.findViewById(android.support.design.R.id.snackbar_text);
+        textView.setTextColor(color);
+
+        snackbar.show();
+    }
+
+
+/*    Show Alert - pass flag to setCancelable() method to cancel alert or not on back press and on touch outside
+      flag true - restrict alert dismiss and false for dialog dismiss
+*/
+    public static void showAlertOK(Context context, String title, String message, String btnText, boolean flag) {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+        alertDialogBuilder.setTitle(title)
+                .setCancelable(flag)
+                .setMessage(message)
+                .setPositiveButton(btnText, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                }).show();
+    }
+
+    /*    Show Alert - pass flag to setCancelable() method to cancel alert or not on back press and on touch outside
+          flag true - restrict alert dismiss and false for dialog dismiss
+    */
+    public static void showAlertOKCancel(final Context context, String title, String message, String posBtnText, String negBtnText, boolean flag) {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+        alertDialogBuilder.setTitle(title)
+                .setCancelable(flag)
+                .setMessage(message)
+                .setPositiveButton(posBtnText, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                })
+                .setNegativeButton(negBtnText, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        ToasterMessage.showToast(context, "Negative Clicked");
+                    }
+                }).show();
+    }
+
 }
